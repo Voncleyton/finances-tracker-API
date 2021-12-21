d63dc73587409c0434167cc774c149581d98f1ac
VOncleyton Martins <martins.voncleyton@gmail.com>
2021-12-21 19:42:58 -0300

    test: ensure AddTransaction return code 400 and an Error when description field is provided

--- src/presentation/controllers/add-transaction-controller.spec.ts
+++ src/presentation/controllers/add-transaction-controller.spec.ts
@@ -126,4 +126,31 @@ describe('Add Transaction Controller - required fields not provided', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('currency'));
   });
+
+  test('Should return 400 and an Error if field [description] is not provided', async () => {
+    const sut = new AddTransactionController();
+
+    const httpRequest = {
+      body: {
+        type: 'expense',
+        date: new Date(),
+        value: 2000,
+        currency: 'real',
+        processed: false,
+        recurring: true,
+        recurring_periodicity: 'monthly',
+        repeat: false,
+        repeat_times: null,
+        repeat_periodicity: null,
+        category: 'salary',
+        tags: [],
+        ignore: false,
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('description'));
+  });
 });
