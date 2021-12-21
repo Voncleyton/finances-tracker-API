aeb7746aa756c6e9cf7a4c05d90dd260ef1797c3
VOncleyton Martins <martins.voncleyton@gmail.com>
2021-12-21 19:23:49 -0300

    feat: ensure AddTransaction return code 400 and an Error when no date is provided

--- src/presentation/controllers/addTransactionController.ts
+++ src/presentation/controllers/addTransactionController.ts
@@ -1,8 +1,19 @@
+import { MissingParamError } from '../errors/missing-paramter-error';
+import { badRequest } from '../helpers/http-helper';
 import { Controller } from '../protocols/controller';
 import { HttpRequest, HttpResponse } from '../protocols/http';
 
 export class AddTransactionController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
+    const { body: fields } = httpRequest;
+    const requiredFields = ['type', 'date'];
+
+    for (const field of requiredFields) {
+      if (!fields[field]) {
+        return badRequest(new MissingParamError(field));
+      }
+    }
+
     return {
       statusCode: 400,
       body: new Error('required field not provided: type'),
