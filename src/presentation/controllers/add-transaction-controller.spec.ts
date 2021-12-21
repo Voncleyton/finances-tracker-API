b9b96fa3a61052cd498f3f7c7c53d18ba3f205b1
VOncleyton Martins <martins.voncleyton@gmail.com>
2021-12-21 19:24:13 -0300

    test: ensure AddTransaction return code 400 and an Error when no date is provided

--- src/presentation/controllers/add-transaction-controller.spec.ts
+++ src/presentation/controllers/add-transaction-controller.spec.ts
@@ -1,49 +1,75 @@
-import { AddTransactionController } from "./addTransactionController";
+import { MissingParamError } from '../errors/missing-paramter-error';
+import { AddTransactionController } from './addTransactionController';
 
 const VALID_TRANSACTION = {
-  type: "expense",
+  type: 'expense',
   date: new Date(),
   value: 2000,
-  currency: "real",
-  description: "salary",
+  currency: 'real',
+  description: 'salary',
   processed: false,
   recurring: true,
-  recurring_periodicity: "monthly",
+  recurring_periodicity: 'monthly',
   repeat: false,
   repeat_times: null,
   repeat_periodicity: null,
-  category: "salary",
+  category: 'salary',
   tags: [],
-  ignore: false
-}
+  ignore: false,
+};
 
-describe('Add Transaction Controller', () => {
-
-  test('Should return 400 and an Error if type is not provided', async () => {
+describe('Add Transaction Controller - required fields not provided', () => {
+  test('Should return 400 and an Error if field [type] is not provided', async () => {
     const sut = new AddTransactionController();
 
     const httpRequest = {
       body: {
         date: new Date(),
         value: 2000,
-        currency: "real",
-        description: "salary",
+        currency: 'real',
+        description: 'salary',
         processed: false,
         recurring: true,
-        recurring_periodicity: "monthly",
+        recurring_periodicity: 'monthly',
         repeat: false,
         repeat_times: null,
         repeat_periodicity: null,
-        category: "salary",
+        category: 'salary',
         tags: [],
-        ignore: false
-      }
-    }
+        ignore: false,
+      },
+    };
 
     const httpResponse = sut.handle(httpRequest);
 
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("required field not provided: type"));
+    expect(httpResponse.body).toEqual(new MissingParamError('type'));
   });
 
+  test('Should return 400 and an Error if field [date] is not provided', async () => {
+    const sut = new AddTransactionController();
+
+    const httpRequest = {
+      body: {
+        type: 'expense',
+        value: 2000,
+        currency: 'real',
+        description: 'salary',
+        processed: false,
+        recurring: true,
+        recurring_periodicity: 'monthly',
+        repeat: false,
+        repeat_times: null,
+        repeat_periodicity: null,
+        category: 'salary',
+        tags: [],
+        ignore: false,
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('date'));
+  });
 });
